21bcb650e131cc8638db95ad7b37f70554163962
Tor Hagemann <tohagema@cisco.com>
2017-05-21 22:24:08 -0700

    package.json: fix `eslint` configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+	extends: [
+		'eslint:recommended',
+	],
+	rules: {
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'never'],
+	},
+}
