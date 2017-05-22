58d2a68f66ef6f9cfd942a6fbe75c6710daced8f
Tor Hagemann <tohagema@cisco.com>
2017-05-21 22:37:12 -0700

    config: use .js instead of .json5

--- config/default.js
+++ config/default.js
@@ -1,4 +1,5 @@
-{
+/* eslint-env node */
+module.exports = {
 	client: {
 		secret: null, // Generate a token at: https://cloud.lifx.com/sign_in
 		// then, copy this file to local.json5 (and fill in client.secret)
